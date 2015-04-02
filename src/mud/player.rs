b82e9c562c3bf1787c0f0ae280bef711c8b0dc44
Jared Miller <jtmiller@gmail.com>
2015-04-02 14:36:59 -0400

    inventory and item junk

--- src/mud/player.rs
+++ src/mud/player.rs
@@ -7,15 +7,25 @@ pub struct Player {
     pub name: String,
     health: i32,
     attack: i32,
+    pub inventory: Inventory,
     pub position: Point
 }
 
 impl Player {
     pub fn new() -> Player {
+
+        let mut items: Vec<Item> = Vec::new();
+
+        let inventory = Inventory {
+            items: items,
+            uses: 10
+        };
+
         Player {
             name: "Jared".to_string(),
             health: 10,
             attack: 10,
+            inventory: inventory,
             position: Point { x: 1, y: 0 }
         }
     }
@@ -24,3 +34,14 @@ impl Player {
         println!("{} attacks for {} damage", self.name, self.attack);
     }
 }
+
+pub struct Inventory {
+    pub items: Vec<Item>,
+    uses: i32
+}
+
+pub struct Item {
+    name: String
+}
+
+
